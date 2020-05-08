b8ce38d396111081c024ea20c62d37706a06d911
loganhersh <47787402+loganhersh@users.noreply.github.com>
2020-05-07 23:44:03 -0400

    incomplete

--- backend/controllers/billing.controller.js
+++ backend/controllers/billing.controller.js
@@ -8,6 +8,11 @@ module.exports = {
 
 
 // Returns billing table data for reservations within 5 days of the current date (before or after)
-function getCurrentInvoices() {
-
+async function getCurrentInvoices(req, res, next) {
+  billingService.getInvoicesInInterval().then(results => {
+    (results) ? res.status(200).json(results) : res.status(404).json({error: "No current invoices found"});
+  })
+  .catch(error => {
+    res.status(400).json({error: "Current invoices could not be retrieved"});
+  });
 }
--- backend/services/billing.service.js
+++ backend/services/billing.service.js
@@ -2,14 +2,33 @@ const db = require('../db/db');
 
 
 module.exports = {
-
+  getInvoicesInInterval
 };
 
 
 // gets billing table data for invoices with check in or check out with a
 // certain number of days of the current date
-function getInvoicesInInterval(days) {
-  const query = "SELECT i.invoice_id AS invoiceId, CONCAT(g.guest_firstname,' ',g.guest_lastname)"
+function getInvoicesInInterval() {
+  const query = "SELECT i.invoice_id AS invoiceId, CONCAT(g.guest_firstname,' ',g.guest_lastname) AS name, g.guest_email AS email, g.guest_phone AS phone, i.amount_paid AS amountPaid, i.total_amount AS totalAmount, (i.total_amount - i.amount_paid) AS amountOwed "
+      + "FROM RESERVATION AS r "
+      + "INNER JOIN INVOICE AS i ON r.reservation_id=i.invoice_id "
+      + "INNER JOIN GUEST AS g ON r.guest_id=g.guest_id "
+      + "WHERE (r.check_in_date <= DATE_ADD(CURRENT_DATE(), INTERVAL 5 DAY) AND r.check_in_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 10 DAY)) OR (r.check_out_date <= DATE_ADD(CURRENT_DATE(), INTERVAL 5 DAY) AND r.check_out_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 10 DAY))"
 
+  return new Promise((resolve, reject) => {
+    db.query(query, (error, results) => {
+      if(error) reject(error);
+      var dataArr = [];
+      if(results.length > 0) {
+        results.forEach(row => {
+          dataArr.push({invoiceId: row.invoiceId, name: row.name, email: row.email,
+          phone: row.phone, amountPaid: row.amountPaid, totalAmount: row.totalAmount, amountOwed: row.amountOwed});
+        });
+        resolve(dataArr);
+      } else {
+        resolve();
+      }
+    });
+  });
 
 }
